fc384c3a874c1440d0a36854c7831aefddcd5780
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2022-01-05 23:04:19 -0300

    Implementing User controller

--- src/main/java/br/com/giovanederenevick/cars/api/exceptions/ExceptionHandlerConfig.java
+++ src/main/java/br/com/giovanederenevick/cars/api/exceptions/ExceptionHandlerConfig.java
@@ -3,17 +3,13 @@ package br.com.giovanederenevick.cars.api.exceptions;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.Set;
 
 @RestControllerAdvice
 public class ExceptionHandlerConfig extends ResponseEntityExceptionHandler {
